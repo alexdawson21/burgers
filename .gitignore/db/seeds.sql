98c5bf51fd4a8e411432f12c09f18bc6d4361ddb
Alex Dawson <alexanderdawson@hotmail.ca>
2018-11-01 13:39:34 -0400

    second

--- .gitignore/db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Veggieburger');
-INSERT INTO burgers (burger_name) VALUES ('Plain burger');
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggieburger');
+INSERT INTO burgers (burger_name) VALUES ('Plain burger');
